f7292be6b6a34d9cf18a75d3ea5c97a27c4836e6
katychuang <katychuang@gmail.com>
2018-06-01 11:33:14 -0400

    include stack and ohmyzsh with new_mac script

--- new_mac.sh
+++ new_mac.sh
@@ -1,9 +1,12 @@
-xcode-select --install 
+xcode-select --install
 
 # install brew
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
 brew doctor
 
+# install oh my #!/usr/bin/env zsh
+sudo curl -L http://install.ohmyz.sh | sh
 
-
+# install haskell Stack
+curl -sSL https://get.haskellstack.org/ | sh
